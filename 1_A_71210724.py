37df366653588f9c38f3aa5130fd46980fd4f880
wulandarykwaitota <93325366+wulandarykwaitota@users.noreply.github.com>
2022-11-25 10:19:28 +0700

    Update 1_A_71210724.py

--- 1_A_71210724.py
+++ 1_A_71210724.py
@@ -1,6 +1,6 @@
 nama = input("Masukkan nama mahasiswa : ")
 nim = input("Masukkan NIM-nya : ")
-string =(nim[0:4])
+
 if nim[0:2] == '71' and int(nim[2:4]) <= 22 and int(nim[2:4]) >= 20:
     
     print(nim, "Merupakan mahasiswa informatika angkatan 20 hingga 22")
