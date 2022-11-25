051872b50fd86a5ed2e8a45977cc3b7c8b578509
wulandarykwaitota <93325366+wulandarykwaitota@users.noreply.github.com>
2022-11-25 10:25:48 +0700

    Update 3_A_71210724.py

--- 3_A_71210724.py
+++ 3_A_71210724.py
@@ -1,10 +1,19 @@
 soal1 = input("Masukkan nilai soal 1: ")
 soal2 = input("Masukkan nilai soal 2: ")
 soal3 = input("Masukkan nilai soal 3: ")
-umur = input("Masukkan umut anda: ")
-rata = int(input("Rata-rata nilai Anda: "))
-if  rata < 80:
-   print(rata,"selamat anda lulus!")
+umur = int(input("Masukkan umur anda: "))
+hasil = (soal1*0.5) + (soal2*0.3) + (soal3*0.2)
+print("Rata-rata nilai Anda: ",hasil)
+
+if umur <= 25:
+    if hasil >= 80:
+        print("selamat anda lulus!")
+    else:
+        print("Coba lagi tahun depan!")
 else:
-   print("Coba lagi  tahun depan!")
+    if hasil >= 90:
+        print("selamat anda lulus!")
+    else:
+        print("Coba lagi tahun depan!")
+
 
